f547819de4e216b1afff67af5d5e127d1bb6ba7b
Daniel Lewinsohn <d_lewinsohn@coloradocollege.edu>
2020-11-10 15:31:09 -0700

    final noise creator

--- noise_creator_v2.py
+++ noise_creator_v2.py
@@ -1,31 +1,23 @@
 import numpy as np
-import random as rand
 from PIL import Image
 
 def randomize_bits(ran):
-    rand.seed(4)
+    np.random.seed(4)
 
-    img = Image.open("gradient.png")
+    in_image = 'gradient.png'
+
+    img = Image.open(in_image)
     img.show()
     mode = img.mode
     size = img.size
-    data = img.tobytes()
+   
+    in_bytes = np.array(img.getdata(), dtype=np.uint8)
+    bits = np.unpackbits(in_bytes)
 
-    #test = np.array(img.tobytes(),dtype=np.uint8)
-    #Image.frombytes(mode,size, data).show()
-    in_image = 'gradient.png'
-    out_image = 'out_gradient.png'
-
-    #in_bytes = np.fromfile(in_image, np.uint8)
-    bits = np.unpackbits(data)
-
-    for i, bit in enumerate(bits):
-        if rand.random() < ran:
-            bits[i] ^= bits[i]
-
+    bits ^= np.random.random(bits.shape) < ran
     
     out_bytes = np.packbits(bits)
-    #out_bytes.tofile(out_image)
     Image.frombytes(mode,size, out_bytes).show()
+
 if __name__ == "__main__":
-    randomize_bits(1)
+    randomize_bits(.1)
